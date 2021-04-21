90eeb974dddbce7a2bdce984047a2122553d06ad
Sean Moir <seanmoir99@gmail.com>
2021-04-22 03:25:58 +1200

    finished mastery test practice 7

--- mastery7/plots.py
+++ mastery7/plots.py
@@ -40,11 +40,44 @@ plt.gca().set_aspect('equal')
 plt.show()
 
 t = np.arange(0,2*np.pi,0.01)
-a = 1
-b = 2
+a = 4
+b = 1
 x = (a - b) * np.cos(t) + b * np.cos(((a - b)/b) * t)
 y = (a - b) * np.sin(t) - b * np.sin(((a - b)/b) * t)
 plt.plot(x,y)
+# the following line makes sure the aspect ratio is 1:1
+plt.gca().set_aspect('equal')
+plt.show()
+
+t = np.arange(0,2*np.pi,0.01)
+x = np.cos(t)
+y = np.sin(t)
+r = [[np.cos(np.pi/4), -np.sin(np.pi/4)], [np.sin(np.pi/4), np.cos(np.pi/4)]]
+pts = np.vstack((x, y))
+out =  np.dot(r, np.dot([[1, 0], [0, 3]], pts))
+plt.plot(out[0], out[1])
+# the following line makes sure the aspect ratio is 1:1
+plt.gca().set_aspect('equal')
+plt.show()
+
+t = np.arange(0,2*np.pi,0.01)
+x = t * np.cos(t)
+y = t * np.sin(t)
+r = [[np.cos(np.pi/4), -np.sin(np.pi/4)], [np.sin(np.pi/4), np.cos(np.pi/4)]]
+pts = np.vstack((x, y))
+out =  np.dot(r, np.dot([[1, 0], [0, 3]], pts))
+plt.plot(out[0], out[1])
+# the following line makes sure the aspect ratio is 1:1
+plt.gca().set_aspect('equal')
+plt.show()
+
+t = np.arange(0,2*np.pi,0.01)
+x = t - np.sin(t)
+y = 1 - np.cos(t)
+r = [[np.cos(np.pi/4), -np.sin(np.pi/4)], [np.sin(np.pi/4), np.cos(np.pi/4)]]
+pts = np.vstack((x, y))
+out =  np.dot(r, np.dot([[1, 0], [0, 3]], pts))
+plt.plot(out[0], out[1])
 # the following line makes sure the aspect ratio is 1:1
 plt.gca().set_aspect('equal')
 plt.show()
