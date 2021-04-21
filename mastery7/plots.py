9d40f94e286d04f3f452295c53213ce0ae378c3e
Sean Moir <seanmoir99@gmail.com>
2021-04-22 03:01:59 +1200

    working through mastery practice 7

--- mastery7/plots.py
+++ mastery7/plots.py
@@ -1,6 +1,54 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+x = np.arange(0, 10,0.1)
+
+y = np.sin(x)
+plt.plot(x, y)
+plt.show()
+
+y = 3 * np.sin(x*x) - 2 * np.cos(x*x*x)
+plt.plot(x, y)
+plt.show()
+
+y = pow(2, ((-x)*2)/2)
+plt.plot(x, y)
+plt.show()
+
+t = np.arange(0,2*np.pi,0.01)
+x = t * np.cos(t)
+y = t * np.sin(t)
+plt.plot(x,y)
+# the following line makes sure the aspect ratio is 1:1
+plt.gca().set_aspect('equal')
+plt.show()
+
+t = np.arange(0,2*np.pi,0.01)
+x = pow(np.e, 0.3*t) * np.cos(t)
+y = pow(np.e, 0.3*t) * np.sin(t)
+plt.plot(x,y)
+# the following line makes sure the aspect ratio is 1:1
+plt.gca().set_aspect('equal')
+plt.show()
+
+t = np.arange(0,2*np.pi,0.01)
+x = t - np.sin(t)
+y = 1 - np.cos(t)
+plt.plot(x,y)
+# the following line makes sure the aspect ratio is 1:1
+plt.gca().set_aspect('equal')
+plt.show()
+
+t = np.arange(0,2*np.pi,0.01)
+a = 1
+b = 2
+x = (a - b) * np.cos(t) + b * np.cos(((a - b)/b) * t)
+y = (a - b) * np.sin(t) - b * np.sin(((a - b)/b) * t)
+plt.plot(x,y)
+# the following line makes sure the aspect ratio is 1:1
+plt.gca().set_aspect('equal')
+plt.show()
+
 a = [[1, 1], [1, 1]]
 b = [[1, 2, 0, 4, 5, 10], [5, 3, 2, 8, 9, 1]]
 
